5473ae629d91e38762afec4da7cd6d47e2086484
ameda-kevin <amedakevin@gmail.com>
2024-02-16 18:18:05 +0300

    everything is working fine

--- src/test/java/com/ameda/kevin/TDD/posts/controller/PostControllerTest.java
+++ src/test/java/com/ameda/kevin/TDD/posts/controller/PostControllerTest.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static java.lang.StringTemplate.STR;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
